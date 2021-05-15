673f1b145850926b7b61c13a1c35deb48dd5f28b
Yaroslav <yaroslavcoworker@gmail.com>
2021-05-15 13:40:17 +0300

    Правки

--- app/src/main/java/me/kolotilov/lets_a_go/ui/map/LocationService.kt
+++ app/src/main/java/me/kolotilov/lets_a_go/ui/map/LocationService.kt
@@ -27,7 +27,7 @@ interface LocationService {
 }
 
 fun getLocationService(context: Context): LocationService {
-    return RxLocationServiceImpl(context)
+    return ReleaseLocationServiceImpl(context)
 }
 
 private const val INTERVAL = 2000L
